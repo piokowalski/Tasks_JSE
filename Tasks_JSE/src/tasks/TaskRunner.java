49f7b4844501b78c8a1f496cff62729f33a746e9
piokowalski <piokowalski@onet.pl>
2019-03-26 11:58:48 +0100

    Tasks manager project - Task22 - exceptions training - w/ scanner

--- Tasks_JSE/src/tasks/TaskRunner.java
+++ Tasks_JSE/src/tasks/TaskRunner.java
@@ -2,7 +2,7 @@ package tasks;
 
 import tasks.tasks.Task20;
 import tasks.tasks.Task21;
-
+import tasks.tasks.Task22;
 
 public class TaskRunner {
     public void run() {
@@ -10,7 +10,10 @@ public class TaskRunner {
 //        Task20 task20 = new Task20();
 //        task20.run();
 
-        Task21 task21 = new Task21();
-        task21.run();
+//        Task21 task21 = new Task21();
+//        task21.run();
+
+        Task22 task22 = new Task22();
+        task22.run();
     }
 }
---
+++ Tasks_JSE/src/tasks/tasks/Task22.java
@@ -0,0 +1,13 @@
+package tasks.tasks;
+
+import java.util.Scanner;
+
+public class Task22 {
+    public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+        int digit = scanner.nextInt();
+
+        System.out.println(digit);
+    }
+}

d2ad8d450ae356c933ae6e39fc544db99006bd0a
piokowalski <piokowalski@onet.pl>
2019-03-26 10:54:37 +0100

    Tasks manager project - Task20 - counting dots

--- Tasks_JSE/src/tasks/tasks/Task20.java
+++ Tasks_JSE/src/tasks/tasks/Task20.java
@@ -1,8 +1,28 @@
 package tasks.tasks;
 
+import java.util.Scanner;
+
 public class Task20 {
     public void run() {
 
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        int dots = countDots(line);
+
+        System.out.println(dots);
     }
-}
+
+        private int countDots(String line) {
+            char[] chars = line.toCharArray();
+            int counter = 0;
+            for (int i=0; i<chars.length; i++) {
+                if (chars[i] == ',') {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+

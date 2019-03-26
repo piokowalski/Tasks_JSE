27015d7cb3f757452c351991c1dc555f16f2ed7d
piokowalski <piokowalski@onet.pl>
2019-03-26 12:20:39 +0100

    Task22 - loop catching only loop, when mistake - repeating the loop

--- Tasks_JSE/src/tasks/tasks/Task22.java
+++ Tasks_JSE/src/tasks/tasks/Task22.java
@@ -1,13 +1,32 @@
 package tasks.tasks;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task22 {
     public void run() {
 
-        Scanner scanner = new Scanner(System.in);
-        int digit = scanner.nextInt();
+        boolean flag = true;
 
-        System.out.println(digit);
+
+
+                Integer result = null;
+        while (result == null) {
+            result = readNumber();
+            if (result != null) {
+                flag = false;
+            }
+        }
+    }
+    public Integer readNumber() {
+        Scanner scanner = new Scanner(System.in);
+        Integer result = null;
+        System.out.println("Give me some digit: ");
+        try {
+            result = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("This is not a digit!!!");
+        }
+        return result;
     }
 }
